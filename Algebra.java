84e602b81a4cf710b8add9f8083b8b63afed0f1c
john-smith8888 <johnsmith8aug88@gmail.com>
2024-11-22 16:53:13 +0200

    Algebra 4th attempt - changed plus/minus negative handling

--- Algebra.java
+++ Algebra.java
@@ -26,7 +26,10 @@ public class Algebra {
 	// Returns x1 + x2
 	public static int plus(int x1, int x2) {
 		for(int i=0;i<x2;i++){
-			x1++;
+			if (x2<0) {
+				x1--;
+			}
+			else x1++;
 		}
 		return x1;
 	}
@@ -34,7 +37,10 @@ public class Algebra {
 	// Returns x1 - x2
 	public static int minus(int x1, int x2) {
 		for(int i=0;i<x2;i++){
-			x1--;
+			if (x2<0) {
+				x1++;
+			}
+			else x1--;
 		}
 		return x1;
 	}
